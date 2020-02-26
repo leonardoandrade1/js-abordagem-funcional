d40e19f3bb83a0c0daf5bdff89fbeb5b91125e91
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-26 15:28:22 -0300

    composicao de funcoes

--- public/app/nota/service.js
+++ public/app/nota/service.js
@@ -1,21 +1,34 @@
 import { handleStatus } from '../utils/promise-helpers.js';
-import { partialize } from '../utils/operators';
+import { partialize } from '../utils/operators.js';
 
 const API = 'http://localhost:3000/notas';
 
 const getItemsFromNotas = notas => notas.$flatMap(nota => nota.itens);
-const filterItemsByCode = code => items => items.filter(item => item.codigo == code);
+const filterItemsByCode = (code, items) => items.filter(item => item.codigo === code);
 const sumItemsValue = items => items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
 
     listAll() {
 
-        return fetch(API).then(handleStatus);
+        return fetch(API)
+            .then(handleStatus)
+            .catch(err => {
+                console.log(err);
+                return Promise.reject('Não foi possível obter as notas fiscais');
+            });
     },
 
     sumItems(code) {
         const filterItems = partialize(filterItemsByCode, code);
-        return this.listAll().then(sumItems(code));
+
+        return this.listAll()
+            .then(notas => 
+                sumItemsValue(
+                    filterItems(
+                        getItemsFromNotas(notas)
+                    )
+                )
+            );
     }
 };
