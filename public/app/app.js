d5f9adde28f1b3a295d7ffa2a17a2414138af206
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-26 16:07:56 -0300

    criando debounce para chamadas de funcoes

--- public/app/app.js
+++ public/app/app.js
@@ -1,17 +1,18 @@
 // app/app.js
 import { notasService as service } from './nota/service.js';
-import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
-import { takeUntil } from './utils/operators.js';
+import { takeUntil, debounceTime } from './utils/operators.js';
 
-const operation = takeUntil(3, () => 
-    service
-    .sumItems('2143')
-    .then(console.log)
-    .catch(console.log)
-)
+const action = debounceTime(500, 
+    takeUntil(3, () => 
+        service
+        .sumItems('2143')
+        .then(console.log)
+        .catch(console.log)
+    )
+);
 
 document
 .querySelector('#myButton')
-.onclick = operation;
+.onclick = action;
     
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -9,3 +9,11 @@ export const pipe = (...fns) => value =>
 
 export const takeUntil = (times, fn) =>
     () => times-- > 0 && fn();
+
+export const debounceTime = (milliseconds, fn) => {
+    let timer = 0;
+    return () => {
+        clearTimeout(timer)
+        timer = setTimeout(fn, milliseconds);
+    }
+}
