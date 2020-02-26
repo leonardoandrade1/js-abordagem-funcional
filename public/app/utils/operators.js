fa7840308f5540f5e2119226e513d21127587856
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-26 15:44:21 -0300

    substituindo o compose por pipe, criacao de funcoes na ordem humana de leitura

--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -3,3 +3,6 @@ export const partialize = (fn, ...args) =>
 
 export const compose = (...fns) => value => 
     fns.reduceRight((previousValue, fn) => fn(previousValue), value);
+
+export const pipe = (...fns) => value => 
+    fns.reduce((previousValue, fn) => fn(previousValue), value);
