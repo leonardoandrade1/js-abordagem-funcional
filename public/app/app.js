a4811b653f221226ac90fe56dc27c8a9e1002445
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-26 15:57:38 -0300

    adicionando limitacao de chamadas para execucao da funcao 'takeUntil'

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,17 @@
 // app/app.js
+import { notasService as service } from './nota/service.js';
 import { log } from './utils/promise-helpers.js';
 import './utils/array-helpers.js';
-import { notasService as service } from './nota/service.js';
+import { takeUntil } from './utils/operators.js';
 
-document
-.querySelector('#myButton')
-.onclick = () => 
+const operation = takeUntil(3, () => 
     service
     .sumItems('2143')
     .then(console.log)
-    .catch(console.log);
+    .catch(console.log)
+)
+
+document
+.querySelector('#myButton')
+.onclick = operation;
+    
