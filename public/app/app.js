34cbeec3cb17de2d4b6e7b24ba31afa2ce818950
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-27 10:33:00 -0300

    06 - implementando eventEmitter para pattern publisher/subscriber

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,8 @@
 import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
 import { takeUntil, debounceTime, partialize, compose } from './utils/operators.js';
-import { timeoutPromise, retry } from './utils/promise-helpers.js'
+import { timeoutPromise, retry } from './utils/promise-helpers.js';
+import { EventEmmiter } from './utils/event-emmiter.js'
 
 const operations = compose(
     partialize(debounceTime, 500),
@@ -11,7 +12,7 @@ const operations = compose(
 
 const action = operations(() => 
     retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(console.log)
+    .then(total => EventEmmiter.emit('itensTotalizados', total))
     .catch(console.log)
 )
 
