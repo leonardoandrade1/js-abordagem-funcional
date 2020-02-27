494144ad8808baf0df262efc72d3eee82f7e2adb
Leonardo de Andrade <andrade.leonardo@telefonica.com>
2020-02-27 10:37:59 -0300

    criando handlers para aplicacao do pattern de publisher/subscriber

--- public/app/app.js
+++ public/app/app.js
@@ -3,7 +3,7 @@ import { notasService as service } from './nota/service.js';
 import './utils/array-helpers.js';
 import { takeUntil, debounceTime, partialize, compose } from './utils/operators.js';
 import { timeoutPromise, retry } from './utils/promise-helpers.js';
-import { EventEmmiter } from './utils/event-emmiter.js'
+import { EventEmitter } from './utils/event-emitter.js'
 
 const operations = compose(
     partialize(debounceTime, 500),
@@ -12,7 +12,7 @@ const operations = compose(
 
 const action = operations(() => 
     retry(3, 3000, () => timeoutPromise(200, service.sumItems('2143')))
-    .then(total => EventEmmiter.emit('itensTotalizados', total))
+    .then(total => EventEmitter.emit('itensTotalizados', total))
     .catch(console.log)
 )
 
